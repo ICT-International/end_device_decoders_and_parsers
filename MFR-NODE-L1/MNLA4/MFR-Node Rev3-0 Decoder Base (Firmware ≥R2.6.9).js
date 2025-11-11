27d0d4db6f2743aeef9c3f7e74f7efd297681898
Toby-ICT-International <toby@ictinternational.com.au>
2025-11-11 14:48:58 +1100

    Update MFR-Node Rev3-0 Decoder Base (Firmware ≥R2.6.9).js
    Added extra detail regarding the use of Nested and Flat decoder types.

--- MFR-NODE-L1/MNLA4/MFR-Node Rev3-0 Decoder Base (Firmware ≥R2.6.9).js
+++ MFR-NODE-L1/MNLA4/MFR-Node Rev3-0 Decoder Base (Firmware ≥R2.6.9).js
@@ -7,7 +7,8 @@
 */
 
 // Structure Type Define, 'nested' or 'flat'
-var TYPE = 'nested';
+var TYPE = 'nested'; //Use nested for Eratos
+//var Type = 'flat'; //Use flat for Meshed. Note: decoded data contains no Unit of Measure. 
 
 /*Class Buffer
 	Purpose: A psuedo buffer class for accessing packet data,
@@ -262,4 +263,5 @@ function primaryDecoder(buf,p){
 	}
 
 	return arr;
+
 }
